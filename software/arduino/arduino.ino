bf048322655ae29813b9fe3fcbb58aac91083dd1
Nathan Lilienthal <nathan@nixpulvis.com>
2013-05-21 15:09:21 -0400

    Handle array of motors.
    All code in the Arduino.ino is basicly just for testing, not going to be
    of any use for actually flying anything.

--- software/arduino/arduino.ino
+++ software/arduino/arduino.ino
@@ -1,88 +1,112 @@
 #include <Servo.h>
 #include "motor.h"
 
-Motor motor_a(9);  // Arduino pin 9.
-Motor motor_b(10); // Arduino pin 10.
+/* Define some constants. */
+#define NUM_MOTORS 4
+
+/* Create the motors. */
+
+Motor motor_a(6);  // Arduino pin 6.  (front-left prop)
+Motor motor_b(9);  // Arduino pin 9.  (back-left prop)
+Motor motor_c(10); // Arduino pin 10. (back-right prop)
+Motor motor_d(11); // Arduino pin 11. (front-right prop)
+
+// Store an array of pointers to the motors.
+Motor* motors[NUM_MOTORS] = { &motor_a, &motor_b, &motor_c, &motor_d };
 
 void setup() {
   Serial.begin(9600);
 
-  // Initialize the motors before anything else. For the ESCs to
-  // operate the arduino must be attached and sending 0% power when
-  // the ESCs gain power. Therefor the ESCs must NOT have power
-  // during this part of the setup.
-  motor_a.initialize();
-  motor_b.initialize();
+  /* Initialize the motors before anything else.
+   * For the ESCs to operate the arduino must be attached and
+   * sending 0% power when the ESCs gain power. Therefor the
+   * ESCs must NOT have power during this part of the setup.
+   */
+  Serial.println("initializing");
+  for (int i = 0; i < NUM_MOTORS; i++) {
+    (*motors[i]).initialize();
+  }
 
   // Now the ESCs may have power.
 }
 
-int incomingByte = 0;   // for incoming serial data
+int incomingByte = 0; // for incoming serial data
 
 void loop() {
   if (Serial.available() > 0) {
-    // read the incoming byte:
     incomingByte = Serial.read();
 
     if (incomingByte == 96) { // key: "`"
       Serial.println("attach");
-      motor_a.attach();
-      motor_b.attach();
+      for (int i = 0; i < NUM_MOTORS; i++) {
+        (*motors[i]).attach();
+      }
 
     } else if (incomingByte == 45) { // key: "-"
       Serial.println("detach");
-      motor_a.detach();
-      motor_b.detach();
+      for (int i = 0; i < NUM_MOTORS; i++) {
+        (*motors[i]).detach();
+      }
 
     } else if (incomingByte == 48) {
       Serial.println(0);
-      motor_a.write(0);
-      motor_b.write(0);
+      for (int i = 0; i < NUM_MOTORS; i++) {
+        (*motors[i]).write(0);
+      }
 
     } else if (incomingByte == 49) {
       Serial.println(1);
-      motor_a.write(1);
-      motor_b.write(1);
+      for (int i = 0; i < NUM_MOTORS; i++) {
+        (*motors[i]).write(1);
+      }
 
     } else if (incomingByte == 50) {
       Serial.println(13);
-      motor_a.write(13);
-      motor_b.write(13);
+      for (int i = 0; i < NUM_MOTORS; i++) {
+        (*motors[i]).write(13);
+      }
 
     } else if (incomingByte == 51) {
       Serial.println(25);
-      motor_a.write(25);
-      motor_b.write(25);
+      for (int i = 0; i < NUM_MOTORS; i++) {
+        (*motors[i]).write(25);
+      }
 
     } else if (incomingByte == 52) {
       Serial.println(37);
-      motor_a.write(37);
-      motor_b.write(37);
+      for (int i = 0; i < NUM_MOTORS; i++) {
+        (*motors[i]).write(37);
+      }
 
     } else if (incomingByte == 53) {
       Serial.println(49);
-      motor_a.write(49);
-      motor_b.write(49);
+      for (int i = 0; i < NUM_MOTORS; i++) {
+        (*motors[i]).write(49);
+      }
 
     } else if (incomingByte == 54) {
       Serial.println(61);
-      motor_a.write(61);
-      motor_b.write(61);
+      for (int i = 0; i < NUM_MOTORS; i++) {
+        (*motors[i]).write(61);
+      }
 
     } else if (incomingByte == 55) {
       Serial.println(73);
-      motor_a.write(73);
-      motor_b.write(73);
+      for (int i = 0; i < NUM_MOTORS; i++) {
+        (*motors[i]).write(73);
+      }
 
     } else if (incomingByte == 56) {
       Serial.println(85);
-      motor_a.write(85);
-      motor_b.write(85);
+      for (int i = 0; i < NUM_MOTORS; i++) {
+        (*motors[i]).write(85);
+      }
 
     } else if (incomingByte == 57) {
       Serial.println(100);
-      motor_a.write(100);
-      motor_b.write(100);
+      for (int i = 0; i < NUM_MOTORS; i++) {
+        (*motors[i]).write(100);
+      }
     }
   }
 }
