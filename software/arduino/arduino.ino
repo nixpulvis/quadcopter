848368108dd05b89d782f3f53959de4ee2da8187
Nathan Lilienthal <nathan@nixpulvis.com>
2013-05-21 15:36:27 -0400

    Apperently Serial.parseInt is a thing...

--- software/arduino/arduino.ino
+++ software/arduino/arduino.ino
@@ -30,83 +30,16 @@ void setup() {
   // Now the ESCs may have power.
 }
 
-int incomingByte = 0; // for incoming serial data
+int buffer[128];    // consistent with arduino's recive buffer.
+int index = 0;
 
 void loop() {
   if (Serial.available() > 0) {
-    incomingByte = Serial.read();
+    int percent = Serial.parseInt();
 
-    if (incomingByte == 96) { // key: "`"
-      Serial.println("attach");
-      for (int i = 0; i < NUM_MOTORS; i++) {
-        (*motors[i]).attach();
-      }
-
-    } else if (incomingByte == 45) { // key: "-"
-      Serial.println("detach");
-      for (int i = 0; i < NUM_MOTORS; i++) {
-        (*motors[i]).detach();
-      }
-
-    } else if (incomingByte == 48) {
-      Serial.println(0);
-      for (int i = 0; i < NUM_MOTORS; i++) {
-        (*motors[i]).write(0);
-      }
-
-    } else if (incomingByte == 49) {
-      Serial.println(1);
-      for (int i = 0; i < NUM_MOTORS; i++) {
-        (*motors[i]).write(1);
-      }
-
-    } else if (incomingByte == 50) {
-      Serial.println(13);
-      for (int i = 0; i < NUM_MOTORS; i++) {
-        (*motors[i]).write(13);
-      }
-
-    } else if (incomingByte == 51) {
-      Serial.println(25);
-      for (int i = 0; i < NUM_MOTORS; i++) {
-        (*motors[i]).write(25);
-      }
-
-    } else if (incomingByte == 52) {
-      Serial.println(37);
-      for (int i = 0; i < NUM_MOTORS; i++) {
-        (*motors[i]).write(37);
-      }
-
-    } else if (incomingByte == 53) {
-      Serial.println(49);
-      for (int i = 0; i < NUM_MOTORS; i++) {
-        (*motors[i]).write(49);
-      }
-
-    } else if (incomingByte == 54) {
-      Serial.println(61);
-      for (int i = 0; i < NUM_MOTORS; i++) {
-        (*motors[i]).write(61);
-      }
-
-    } else if (incomingByte == 55) {
-      Serial.println(73);
-      for (int i = 0; i < NUM_MOTORS; i++) {
-        (*motors[i]).write(73);
-      }
-
-    } else if (incomingByte == 56) {
-      Serial.println(85);
-      for (int i = 0; i < NUM_MOTORS; i++) {
-        (*motors[i]).write(85);
-      }
-
-    } else if (incomingByte == 57) {
-      Serial.println(100);
-      for (int i = 0; i < NUM_MOTORS; i++) {
-        (*motors[i]).write(100);
-      }
+    for (int i = 0; i < NUM_MOTORS; i++) {
+      Serial.println(percent);
+      (*motors[i]).write(percent);
     }
   }
 }
