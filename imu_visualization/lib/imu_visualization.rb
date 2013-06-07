eb72ea365e877f188d0399d64f3cfbf0d7ae9130
Nathan Lilienthal <nathan@nixpulvis.com>
2013-06-07 09:34:53 -0400

    Adding the beginnings of the IMU visualization.
    With OpenGL we can somewhat easily draw 3D objects and move them around.
    We'll be making a box to represent the IMU for the quadcopter. Currently
    I have the box rotating on all 3 axises.

---
+++ imu_visualization/lib/imu_visualization.rb
@@ -0,0 +1,47 @@
+require 'imu_visualization/graphics_engine'
+
+class IMUVisualization < GraphicsEngine
+  attr_reader :rotate_x, :rotate_y, :rotate_z
+
+  def setup
+    # 3D rotation in degrees.
+    @rotate_x = 0
+    @rotate_y = 0
+    @rotate_z = 0
+  end
+
+  def draw
+    # Rotate to match `rotate_x` and `rotate_y`.
+    glRotatef(@rotate_x, 1.0, 0.0, 0.0)
+    glRotatef(@rotate_y, 0.0, 1.0, 0.0)
+    glRotatef(@rotate_z, 0.0, 0.0, 1.0)
+
+    # Draw a box.
+    drawBox(0.5, 0.15, 0.65)
+  end
+
+  def keyboard(key, x, y)
+    if key == 'd'
+      @rotate_y = (@rotate_y + 5) % 360
+    elsif key == 'a'
+      @rotate_y = (@rotate_y - 5) % 360
+    elsif key == 'w'
+      @rotate_x = (@rotate_x + 5) % 360
+    elsif key == 's'
+      @rotate_x = (@rotate_x - 5) % 360
+    elsif key == 'r'
+      @rotate_z = (@rotate_z + 5) % 360
+    elsif key == 'l'
+      @rotate_z = (@rotate_z - 5) % 360
+    elsif key == ' '
+      puts self
+    elsif key == "\e"
+      exit(0)
+    end
+  end
+
+  def to_s
+    "X: #{rotate_x}, Y: #{rotate_y}, Z: #{rotate_z}"
+  end
+
+end
---
+++ imu_visualization/lib/imu_visualization/graphics_engine.rb
@@ -0,0 +1,96 @@
+require 'opengl'
+
+class GraphicsEngine
+  include Gl, Glu, Glut
+
+  def initialize(title, width, height)
+    glutInit
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(width, height)
+    glutInitWindowPosition(100, 100)
+    glutCreateWindow(title)
+
+    # Basic setup
+    glClearDepth(1)
+    glEnable(GL_DEPTH_TEST)
+
+    setup if defined? setup
+
+    glutDisplayFunc(-> do
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+      glLoadIdentity
+
+      draw
+
+      glFlush
+      glutSwapBuffers
+    end) if defined? draw
+
+    glutKeyboardFunc(-> (key, x, y) do
+      keyboard(key, x, y)
+
+      glutPostRedisplay
+    end) if defined? keyboard
+
+    glutMainLoop
+  end
+
+  private
+
+  def drawBox(width, height, depth)
+    # Front surface. (WHITE)
+    glBegin(GL_POLYGON)
+    glColor3f(1.0, 1.0, 1.0)
+    glVertex3f( width, -height, -depth)  # P1 is red
+    glVertex3f( width,  height, -depth)  # P2 is green
+    glVertex3f(-width,  height, -depth)  # P3 is blue
+    glVertex3f(-width, -height, -depth)  # P4 is purple
+    glEnd
+
+    # Back surface. (GREY)
+    glBegin(GL_POLYGON)
+    glColor3f(0.5, 0.5, 0.5)
+    glVertex3f( width, -height, depth)
+    glVertex3f( width,  height, depth)
+    glVertex3f(-width,  height, depth)
+    glVertex3f(-width, -height, depth)
+    glEnd
+
+    # Right surface. (RED)
+    glBegin(GL_POLYGON)
+    glColor3f(1.0, 0.0, 0.0)
+    glVertex3f(width, -height, -depth)
+    glVertex3f(width,  height, -depth)
+    glVertex3f(width,  height,  depth)
+    glVertex3f(width, -height,  depth)
+    glEnd
+
+    # Left surface. (RED)
+    glBegin(GL_POLYGON)
+    glColor3f(1.0, 0.0, 0.0)
+    glVertex3f(-width, -height,  depth)
+    glVertex3f(-width,  height,  depth)
+    glVertex3f(-width,  height, -depth)
+    glVertex3f(-width, -height, -depth)
+    glEnd
+
+    # Top surface. (GREEN)
+    glBegin(GL_POLYGON)
+    glColor3f(0.0, 1.0, 0.0)
+    glVertex3f( width,  height,  depth)
+    glVertex3f( width,  height, -depth)
+    glVertex3f(-width,  height, -depth)
+    glVertex3f(-width,  height,  depth)
+    glEnd
+
+    # Bottom surface. (BLUE)
+    glBegin(GL_POLYGON)
+    glColor3f(0.0, 0.0, 1.0)
+    glVertex3f( width, -height, -depth)
+    glVertex3f( width, -height,  depth)
+    glVertex3f(-width, -height,  depth)
+    glVertex3f(-width, -height, -depth)
+    glEnd
+  end
+
+end
