7deaf2763620250524f8df0b2d5e74a89d9854fa
Alex Johnson <alex.johnson@evertrue.com>
2013-06-06 21:59:42 -0400

    Beginning serial monitor.
    https://trello.com/c/w0dtCBVH

---
+++ serial_monitor/lib/serial_monitor.rb
@@ -0,0 +1,37 @@
+require 'serialport'
+
+class SerialMonitor
+  attr_reader :serial_port, :baud
+  attr_accessor :start_byte, :stop_byte
+
+  def initialize(serial_port, baud, start_byte, stop_byte = start_byte)
+    @serial_port = serial_port
+    @baud = baud
+    @start_byte = start_byte
+    @stop_byte = stop_byte
+
+    # Initialize SerialPort from gem.
+    @sp = SerialPort.new(serial_port, baud)
+  end
+
+
+  def self.open(serial_port, baud, start_byte, stop_byte = start_byte)
+    sm = SerialMonitor.new(serial_port, baud, start_byte, stop_byte)
+
+    if block_given?
+      yield sm
+      sm.close
+    else
+      sm
+    end
+  end
+
+  def gets
+    until @sp.getc == start_byte; end
+    @sp.gets(stop_byte).gsub(stop_byte, "")
+  end
+
+  def close
+    @sp.close
+  end
+end
