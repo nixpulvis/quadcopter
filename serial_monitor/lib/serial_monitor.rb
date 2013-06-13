dd0c172b8d2b0af570c8b119ebe53aefff2ce2b2
Nathan Lilienthal <nathan@nixpulvis.com>
2013-06-13 13:18:22 -0400

    SerialMonitor extends SerialPort.
    fixes #11
    
    By simply extending serialport we get all it's functionality. Then we
    simply override what we need. In this case, for now we just need to wait
    a short amount of time, and then flush the input stream.

--- serial_monitor/lib/serial_monitor.rb
+++ serial_monitor/lib/serial_monitor.rb
@@ -1,41 +1,11 @@
 require 'serialport'
+require 'io/console'
 
-class SerialMonitor
-  attr_reader :serial_port, :baud
-  attr_accessor :start_byte, :stop_byte
-
-  def initialize(serial_port, baud, start_byte, stop_byte = start_byte)
-    @serial_port = serial_port
-    @baud = baud
-    @start_byte = start_byte
-    @stop_byte = stop_byte
-
-    # Initialize SerialPort from gem.
-    @sp = SerialPort.new(serial_port, baud)
-  end
-
-
-  def self.open(serial_port, baud, start_byte, stop_byte = start_byte)
-    sm = SerialMonitor.new(serial_port, baud, start_byte, stop_byte)
-
-    if block_given?
-      yield sm
-      sm.close
-    else
-      sm
-    end
-  end
-
-  def gets
-    begin
-      until @sp.getc == start_byte; end
-      @sp.gets(stop_byte).gsub(stop_byte, "")
-    rescue ArgumentError => e
-      warn "Error parsing serial,` #{e.message}."
-    end
-  end
-
-  def close
-    @sp.close
+class SerialMonitor < SerialPort
+  def self.new(serial_port, baud)
+    sp = super(serial_port, baud)
+    sleep(0.1)
+    sp.iflush
+    sp
   end
 end
