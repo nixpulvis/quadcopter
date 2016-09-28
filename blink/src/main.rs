96448b357cf6c6d8349998c36592bcc2fe37cc5e
Nathan Lilienthal <nlilienthal@apple.com>
2016-09-28 10:34:57 -0700

    Starting the rust project.

---
+++ blink/src/main.rs
@@ -0,0 +1,19 @@
+// https://doc.rust-lang.org/book/no-stdlib.html
+#![feature(lang_items)]
+#![feature(start)]
+#![no_std]
+
+// Pull in the system libc library for what crt0.o likely requires
+extern crate libc;
+
+// Entry point for this program
+#[start]
+fn start(_argc: isize, _argv: *const *const u8) -> isize {
+    0
+}
+
+// These functions and traits are used by the compiler, but not
+// for a bare-bones hello world. These are normally
+// provided by libstd.
+#[lang = "eh_personality"] extern fn eh_personality() {}
+#[lang = "panic_fmt"] extern fn panic_fmt() -> ! { loop {} }
