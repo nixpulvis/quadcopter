2b7f62f1a70bf7dcebae91bb839aeeadaf96aa0b
Nathan Lilienthal <nathan@nixpulvis.com>
2016-11-29 10:14:14 -0700

    Update build to not use avr.

--- build.rs
+++ build.rs
@@ -2,9 +2,8 @@ extern crate gcc;
 
 fn main() {
     // TODO: Use latexmk directly.
-    make::compile_library("spec.pdf", "hardware");
+    make::compile_library("spec.pdf", "doc");
     gcc::compile_library("libhello.a", &["src/hello.c"]);
-    avr_gcc::compile_library("libesc.a", "esc/src/esc.c");
 }
 
 mod make {
@@ -17,26 +16,5 @@ mod make {
             .status()
             .unwrap();
         assert!(exit.success());
-    }
-}
-
-mod avr_gcc {
-    use gcc;
-
-    pub fn compile_library(target: &str, file: &str) {
-        // TODO: ESC project.
-        gcc::Config::new()
-            .compiler("avr-gcc")
-            .target("avr")
-            .file(file)
-            .define("F_CPU", Some("16000000UL"))
-            .define("MMCU", Some("atmega328p"))
-            .flag("-std=c99")
-            .flag("-pedantic")
-            .flag("-Wall")
-            // .flag("-Werror")
-            .include("/usr/local/avr/include")
-            .include("/usr/local/Cellar/avr-libc/2.0.0/avr/include")
-            .compile(target);
     }
 }
